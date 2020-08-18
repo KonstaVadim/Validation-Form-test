412501663fcece3e8c4e0609dd204125471c84d7
Konstantinov <v.konstantinov@pfsz.ru>
2020-08-18 09:56:41 +0300

    Добавил пустые компоненты. сверстал хедер

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
